3460f3e5f70d4abf417547f6ce1c3e1cddcf1ef5
VictoriaReyes <reyedvictoria1803@gmail.com>
2025-07-31 00:02:47 -0600

    module logs

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 // src/lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://guikspbicskovcmvfvwb.supabase.co";
-const SUPABASE_ANON_KEY = "sb_secret_SG4tyyhGy1_1Fdmgod1a4g_VIq4pftg";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL ;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
